6f87a4210c6e44f42bbc949158da8c2f7c860669
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2022-11-21 14:29:07 -0500

    Update Task1.c

--- ProgrammingAssignment6/Task1.c
+++ ProgrammingAssignment6/Task1.c
@@ -1,3 +1,12 @@
+/**************************************************************************************************
+ * Title:               Assignment #6 Task 1
+ * Author:              Jaleel Rogers
+ * Email:               jrogers1239@floridapoly.edu
+ * Creation date:       03/19/2022 @ 10:23 AM
+ * Modification date:   03/19/2022 @ 10:27 AM
+ * Description:         Write a C program that has a function called print_hello that prints the message "Hello".
+ *                      Call the print_hello function four times from the main.
+ **************************************************************************************************/
 #include <stdio.h>
 
 void print_rectangle(int length, int width) // Void is used to indicate a return type, point is universal
