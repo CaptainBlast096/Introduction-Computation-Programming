799279d61d158deace233a40d8e80a87ce0ac21c
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2022-11-21 13:29:50 -0500

    Update and rename main.c to ProgrammingAssignment1.c

---
+++ ProgrammingAssignment1.c
@@ -0,0 +1,30 @@
+/************************************************************
+* Name: ---------  ---------
+* Programming Assignment 1
+* Date: 01-19-2022
+* Description:   This is the first program in C The
+*                code prints "Hello, World!" on the
+*                screen.
+*************************************************************/
+
+// Header Files
+#include <stdio.h>
+
+// main funcion is the entry poin to the program
+int main(int argc, char *argv[])
+{
+    // printf is a function in the standard library I/O 
+    // header, and it prints the values on the standard output.
+    printf("Hello, World!\n");
+
+   
+    
+    printf("Florida Polytechnic University\n");
+    printf("Student ID Card\n\n");
+    printf("Name: John Smith\n");
+    printf("Credit Hours: 22\n");
+    printf("GPA: 1.67\n");
+    
+     return 0; // Inform the operating system that the 
+              //exit is successfull
+} // the end of the main function
