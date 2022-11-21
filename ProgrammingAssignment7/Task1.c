ddd6cecd319441ae080d752bb89414a843e94164
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2022-11-21 14:17:17 -0500

    Update Task1.c

--- ProgrammingAssignment7/Task1.c
+++ ProgrammingAssignment7/Task1.c
@@ -1,13 +1,3 @@
-/***************************************************************************
- * Title:               Assignment #7 Task 1
- * Author:              Jaleel Rogers
- * Email:               jrogers1239@floridapoly.edu
- * Creation date:       03/28/2022 @ 10:00 PM
- * Modification date:   03/28/2022 @ 10:21 PM
- * Description:         Write a C program using the for statement and the functions
- *                      from the standard C library to print the table below. The range of values of x starts from
- *                      −π to π with a step of π/5."
- **************************************************************************/
 #include <stdio.h>
 #include <math.h> // Besides, stdio.h, math.h is needed for the functions
 
