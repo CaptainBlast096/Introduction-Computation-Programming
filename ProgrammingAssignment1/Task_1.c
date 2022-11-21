f2a8322e2703aa891a887db86d6aeed288d3a9c1
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2022-11-21 13:47:53 -0500

    Update and rename ProgrammingAssignment1.c to ProgrammingAssignment1/Task_1.c

---
+++ ProgrammingAssignment1/Task_1.c
@@ -0,0 +1,13 @@
+// Header Files
+#include <stdio.h>
+
+// main funcion is the entry poin to the program
+int main(int argc, char *argv[])
+{
+    // printf is a function in the standard library I/O 
+    // header, and it prints the values on the standard output.
+    printf("Hello, World!\n");
+
+    return 0; // Inform the operating system that the 
+              //exit is successfull
+} // the end of the main function
